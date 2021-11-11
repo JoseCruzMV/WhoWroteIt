8b3c0b3529fdd0fc8b683d65d72a55238dda1b11
Jose C Morgado Vargas <josecruzmv@outlook.com>
2021-11-10 19:10:48 -0600

    Created NetworkUtils class to make the conection with the API

--- app/src/main/java/com/example/whowroteit/FetchBook.java
+++ app/src/main/java/com/example/whowroteit/FetchBook.java
@@ -1,2 +1,27 @@
-package com.example.whowroteit;public class FetchBook {
+package com.example.whowroteit;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+
+    private WeakReference<TextView> mTitleText;
+    private WeakReference<TextView> mAuthorText;
+
+    public FetchBook(TextView mTitleText, TextView mAuthorText) {
+        this.mTitleText = new WeakReference<TextView>(mTitleText);
+        this.mAuthorText = new WeakReference<TextView>(mAuthorText);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
 }
