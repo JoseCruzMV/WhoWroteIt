3ce50ba3adbd947a527455e701227f46f5cc12a2
Jose C Morgado Vargas <josecruzmv@outlook.com>
2021-11-18 20:15:02 -0600

    Handling no network error and no query error

--- app/src/main/java/com/example/whowroteit/MainActivity.java
+++ app/src/main/java/com/example/whowroteit/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.whowroteit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -27,14 +29,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void searchBooks(View view) {
         String queryString = mBookInput.getText().toString();
-        new FetchBook(mTitleText, mAuthorText).execute(queryString);
 
-        mTitleText.setText(R.string.loading);
         InputMethodManager inputManager = (InputMethodManager)
                 getSystemService(Context.INPUT_METHOD_SERVICE);
         if (inputManager != null) {
             inputManager.hideSoftInputFromWindow(view.getWindowToken(),
             InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (manager != null) {
+            networkInfo = manager.getActiveNetworkInfo();
+        }
+
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0 ) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
         }
     }
 }
